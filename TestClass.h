9eab31b40e331fc54600190c64a08d4c7eba221d
sangau001 <sanju.dutta@gmail.com>
2015-01-02 20:46:02 +0530

    Added TestClass references

---
+++ TestClass.h
@@ -0,0 +1,13 @@
+//
+//  TestClass.h
+//  PlayList
+//
+//  Created by Sanjeev Dutta on 12/5/14.
+//  Copyright (c) 2014 Sanjeev Dutta. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface TestClass : NSObject
+
+@end
---
+++ TestClass.m
@@ -0,0 +1,15 @@
+//
+//  TestClass.m
+//
+//  Created by Sanjeev Dutta on 12/5/14.
+//  Copyright (c) 2014 Sanjeev Dutta. All rights reserved.
+//
+
+#import "TestClass.h"
+
+@interface TestClass ()
+@end
+
+@implementation TestClass
+
+@end
